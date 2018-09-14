6d7e61165ed523b777140082b2089cc052ec3644
beer-on-ice <li997477295@outlook.com>
2018-09-14 17:43:15 +0800

    node启动打包后文件

---
+++ vue_163Music/server/app.js
@@ -0,0 +1,35 @@
+const Koa = require('koa')
+const router = require('koa-router')()
+const static = require('koa-static')
+const proxy = require('http-proxy-middleware')
+const path = require('path')
+const app = new Koa()
+
+app.use(static(path.join(__dirname, '../dist')))
+
+app.use(async (ctx, next) => {
+  ctx.respond = false
+  if (ctx.url.startsWith('/api')) {
+    return proxy({
+      target: 'http://192.168.0.61:3000', // 服务器地址
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+      pathRewrite: {
+        '^/api': ''
+      }
+    })(ctx.req, ctx.res, next)
+  }
+  return next()
+})
+
+app.use(async (ctx) => {
+  ctx.body = 'hello world'
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(8088, () => {
+  console.log('server start at port: 8088')
+})
