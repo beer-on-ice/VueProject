ed182765dca0f3d88d4ee518dbd7890db8b9b8a9
coder-LiJin <li997477295@outlook.com>
2018-12-24 21:51:37 +0800

    修改本地端口

--- vue_163Music/server/app.js
+++ vue_163Music/server/app.js
@@ -4,6 +4,7 @@ const static = require('koa-static')
 const proxy = require('http-proxy-middleware')
 const path = require('path')
 const app = new Koa()
+const port = 3001
 
 app.use(static(path.join(__dirname, '../dist')))
 
@@ -11,7 +12,7 @@ app.use(async (ctx, next) => {
   ctx.respond = false
   if (ctx.url.startsWith('/api')) {
     return proxy({
-      target: 'http://192.168.0.61:3001', // 服务器地址
+      target: 'http://192.168.2.109:3000', // 服务器地址
       changeOrigin: true,
       secure: false,
       ws: true,
@@ -30,6 +31,6 @@ app.use(async (ctx) => {
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-app.listen(3000, () => {
-  console.log('server start at port: 3000')
+app.listen(port, () => {
+  console.log('server start at port: ' + port)
 })
--- vue_163Music/vue.config.js
+++ vue_163Music/vue.config.js
@@ -30,7 +30,7 @@ module.exports = {
     // host: '192.168.0.61',
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'http://localhost:3001/',
         changeOrigin: true,
         ws: true,
         pathRewrite: {
