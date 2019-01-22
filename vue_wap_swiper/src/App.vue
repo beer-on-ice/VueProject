ab54cf778f2360db363601b89eb13fcc5036c163
beer-on-ice <li997477295@outlook.com>
2019-01-22 15:17:24 +0800

    整合

---
+++ vue_wap_swiper/src/App.vue
@@ -0,0 +1,19 @@
+<template  lang="pug">
+  #app
+    router-view
+    //- tab(@tabChange="tabSwitch" :current="current")
+</template>
+
+<script>
+import Tab from 'components/tab.vue'
+export default {
+  data () {
+    return {
+      current: 0
+    }
+  }
+}
+</script>
+
+<style>
+</style>
